efaea708b6ac4723ac6f43b837d4519e81cb7ca1
micamilano <micaa.milano@gmail.com>
2021-07-20 23:18:33 -0300

    components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,30 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import Nav from "./Nav";
+import Acciones from "./Acciones";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <p>HOME</p>
+      <Nav></Nav>
+      <header>
+        <div>
+          <h1>
+            WHERES'S MY BO
+            <img
+              id="book-icon"
+              src="https://cdn.icon-icons.com/icons2/809/PNG/512/book_dictionary_icon-icons.com_66121.png"
+            ></img>
+            K
+          </h1>
+          <button type="button" class="btn btn-primary">
+            Leer más...
+          </button>
+        </div>
+      </header>
+      <Acciones></Acciones>
+      <Footer></Footer>
     </div>
   );
 }
