561652511a79f64aa1dd8c38893a81e7cc568ad1
micamilano <micaa.milano@gmail.com>
2021-07-22 17:46:03 -0300

    css + persona/formulario

--- src/components/Persona.jsx
+++ src/components/Persona.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Listado from "./ListadoDePersonas";
 import Formulario from "./FormularioPersonas";
+import { Link } from "react-router-dom";
 
 const Persona = () => {
   const [personas, setPersonas] = useState([]);
@@ -14,7 +15,7 @@ const Persona = () => {
     <>
       <div class="container">
         <button type="button" class="btn btn-primary">
-          Formulario
+          <Link to="/persona/formulario">FORMULARIO</Link>
         </button>
         <button type="button" class="btn btn-primary">
           {personas && <Listado personas={personas}></Listado>}
