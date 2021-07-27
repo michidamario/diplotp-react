8d888b5343b02e1e5a028aa3494b0efad4bd8f5a
sergio120978 <85079942+sergio120978@users.noreply.github.com>
2021-07-27 06:04:59 -0300

    Se agregaron alert

---
+++ src/components/ListadoDePersonas.jsx
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import swal from  'sweetalert';
+
+
+const Listado= ()=>{
+    const [personas, setPersonas] = useState([]);
+    const [libros, setLibros] = useState([]);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleDelete=async (personaId)=>{
+        try{
+            await axios.delete("http://localhost:8080/persona/"+ personaId)
+            swal({
+                text: "Registro eliminado correctamente.",
+                icon: "success"
+                });            
+        }
+        catch(e){
+            
+            swal({
+                text: "No se puede eliminar, tiene libros asociados.",
+                icon: "warning"
+                });
+        }
+    }
+
+
+    const handleEdit= (personaId)=>{
+        const personaAEditar = personas.find(persona=>persona.id == personaId);
+        console.log(personaAEditar);
+    }
+
+    useEffect(async () => {
+        const response = await axios.get("http://localhost:8080/persona");
+        const responseLibros = await axios.get("http://localhost:8080/libro");
+
+        setLibros(responseLibros.data);
+        setPersonas(response.data);
+    }, []);
+
+    return(
+        <div className="containerPersona">
+        <Link to="/persona/formulario">FORMULARIO</Link>
+        {personas && libros && personas.map(persona=>{
+
+            return(
+                <div className="container">
+                    <p>Nombre: {persona.nombre}</p>
+                    <p>Apellido: {persona.apellido}</p>
+                    <p>Alias: {persona.alias}</p>
+                    <p>Email: {persona.email}</p>
+                    <p>Libros: {libros.filter(libro=>{
+                            return libro.persona_id == persona.id
+                        })
+                        .map((libro, i)=>{
+                            if (i>0){
+                                return ", " + libro.nombre
+                            }
+                            return libro.nombre
+                        })} 
+                    </p>
+                    <button onClick={()=>handleEdit(persona.id)} className="btn btn-warning"> Editar</button>
+                    <button onClick={()=>{handleDelete(persona.id)}} className="btn btn-danger">Borrar</button>
+                </div>
+            )
+        })}
+        </div>
+    )
+}
+
+
+
+
+
+export default Listado;
